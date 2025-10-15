7d8b7020b6fab82898438c34accdaa34c5f3c26b
waseem-3 <76593304+waseem-3@users.noreply.github.com>
2025-10-15 16:34:03 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the 2nd number"))
+
+print(f"Addition: {a+b}")
+print(f"Multiplication: {a*b}")
+print(f"Division: {a/b}")
+print(f"Subtraction: {a-b}")
